0fafa9634a08eefe6f84050c14aa125d6dfdecea
Amit Tiwari <70047168+Amit-Tiwari1@users.noreply.github.com>
2023-10-21 15:29:40 +0530

    Add files via upload
    it contains impotant about array

---
+++ aray1.js
@@ -0,0 +1,19 @@
+const array = [1,2,3,4,5,true,"Amit"]
+console.log(array[2])
+
+const array2 = ["amti","anshu","naman"]
+const array3 = new Array("ram","krish","bole")
+
+// array methords
+// push last me ek array add kr deta hai and pop ek last ka element remove kr deta hai 
+// shift and unshift shift remove fisrt value of array and unshift add
+// join hamare array ko string me convert kr deta hai 
+// slice and splice (slice hmesha starting and end le kr ek array return kr deta hai or splice original array me se us value ko hi nikl leta hai)
+// array me agar array push krenge to array ke ander array a jta  hai 
+// or agr concate krenge to array ke andr array ki value add hoke ek nya array return kregi
+// spread operator kisi bhi variable ko three dot ...variable name de kr add kr dete hai
+// flat array agr array ke andr array and array ke andr or bhi array, simple flat use kr ke infinity de kr ye ek naya array return kr dega
+// of,from,isArray- isArray puchta hai ki ye array hai ki nhi, from array me conver karne ke liye.. of se bhi kisi ko bhi array me conver kr deta hai
+
+array.push("anshu")
+console.log(array[7]);
